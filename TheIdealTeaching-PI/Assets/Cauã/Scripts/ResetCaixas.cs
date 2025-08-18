0fefceb045c6cbfd0b79e65e25fb24d9da9a5966
CauaMiguelSS <caua.santos9@alunos.sc.senac.br>
2025-08-18 14:38:58 -0300

    Reset de caixas

---
+++ TheIdealTeaching-PI/Assets/Cauã/Scripts/ResetCaixas.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class ResetCaixas : MonoBehaviour
+{
+    public GameObject[] caixas;
+
+    
+    private Vector3[] posicoesOriginais;
+
+    void Start()
+    {
+        posicoesOriginais = new Vector3[caixas.Length];
+        for (int i = 0; i < caixas.Length; i++)
+        {
+            posicoesOriginais[i] = caixas[i].transform.position;
+        }
+    }
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ResetarCaixas();
+        }
+    }
+
+    void ResetarCaixas()
+    {
+        for (int i = 0; i < caixas.Length; i++)
+        {
+            caixas[i].transform.position = posicoesOriginais[i];
+        }
+    }
+}
