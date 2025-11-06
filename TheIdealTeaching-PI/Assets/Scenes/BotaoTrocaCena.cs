94aed6e170530c437e01804aa93d6bcc9dcf0a3d
CauaMiguelSS <caua.santos9@alunos.sc.senac.br>
2025-11-06 14:39:35 -0300

    Criação da cena de creditos

---
+++ TheIdealTeaching-PI/Assets/Scenes/BotaoTrocaCena.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotaoTrocaCena : MonoBehaviour
+{
+    [Header("Configuração")]
+    [SerializeField] private bool sairDoJogo = false;   
+    [SerializeField] private string nomeCenaDestino = "";  
+
+    public void ExecutarAcao()
+    {
+        if (sairDoJogo)
+        {
+            SairDoJogo();
+        }
+        else if (!string.IsNullOrEmpty(nomeCenaDestino))
+        {
+            TrocarCena();
+        }
+    }
+
+    private void TrocarCena()
+    {
+        SceneManager.LoadScene(nomeCenaDestino);
+    }
+
+    private void SairDoJogo()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
+
+
